1599d948223dcd7c77e693aa62334ce965a51688
tancw <changwei114@126.com>
2016-06-17 16:53:50 +0800

    增加Filter

---
+++ src/main/java/com/capsui/servelt/filter/CharactorEncodingFiler.java
@@ -0,0 +1,38 @@
+package com.capsui.servelt.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Created by tancw on 2016/6/17.
+ */
+public class CharactorEncodingFiler implements Filter {
+	private static final Log log = LogFactory.getLog(CharactorEncodingFiler.class);
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		log.info("编码Filter");
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+}
