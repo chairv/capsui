19162e77972097beb3c545facc25bf68c14a5785
tancw <changwei114@126.com>
2016-06-17 15:57:51 +0800

    the first 提供springmvc

---
+++ src/main/java/com/capsui/web/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.capsui.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by tancw on 2016/6/17.
+ */
+@Controller
+public class IndexController {
+
+	@RequestMapping("home")
+	public String home() {
+		return "resources/html/index.html";
+	}
+}
--- src/main/java/web/controller/IndexController.java
+++
@@ -1,7 +0,0 @@
-package web.controller;
-
-/**
- * Created by tancw on 2016/6/17.
- */
-public class IndexController {
-}
