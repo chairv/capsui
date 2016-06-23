3db8aa4ca0e48ef48e44ef82ee28020b6e2ed44b
tancw <changwei114@126.com>
2016-06-23 18:42:53 +0800

    加入登录

--- src/main/java/com/capsui/dao/mapper/UserMapper.java
+++ src/main/java/com/capsui/dao/mapper/UserMapper.java
@@ -2,9 +2,11 @@ package com.capsui.dao.mapper;
 
 import com.capsui.bean.User;
 import com.capsui.dao.BaseDao;
+import com.capsui.dao.MybatisDao;
 
 /**
  * Created by tancw on 2016/6/23.
  */
+@MybatisDao
 public interface UserMapper extends BaseDao<User> {
 }
--- src/main/java/com/capsui/service/impl/UserServiceImpl.java
+++ src/main/java/com/capsui/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.capsui.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.capsui.bean.User;
 import com.capsui.dao.mapper.UserMapper;
@@ -9,6 +10,7 @@ import com.capsui.service.UserService;
 /**
  * Created by tancw on 2016/6/23.
  */
+@Service
 public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserMapper userMapper;
